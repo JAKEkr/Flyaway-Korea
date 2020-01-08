ab981fbeca82705ed64c4df3bf7e92d5f176f2a0
Jake Lee <secujake@gmail.com>
2020-01-08 10:36:39 +0900

    Init Server-side code

---
+++ Server/app.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const express = require('express');
+const http = require('http');
+const logger = require('winston');
+
+const app = express();
+
+app.use(express.json());
+
+// Set TCP port
+const port = process.env.PORT || '8888';
+app.set('port', port);
+
+// Set server
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+    if (error.syscall !== 'listen')
+        throw error;
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+} // onError()
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    logger.('Listening on ' + bind);
+} // onListening()
