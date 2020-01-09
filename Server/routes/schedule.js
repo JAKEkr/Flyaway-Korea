a576efbd4ba954a7bdbabdea7f790f869b82d93a
Jake Lee <secujake@gmail.com>
2020-01-09 17:07:12 +0900

    Add schema about flight schedules

---
+++ Server/routes/schedule.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const router = require('express').Router();
+
+router.get('/arrivals', (req, res) => {
+
+});
+
+router.get('/departures', (req, res) => {
+
+});
+
+module.exports = router;
