285cd7b4a2910386271f1917cde6b56f3590c25a
Eunji Lee <ej_0703@naver.com>
2020-01-09 21:07:05 +0900

    Init client project

---
+++ Client/FlyAway/resource.h
@@ -0,0 +1,26 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// FlyAway.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDC_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FLYAWAY_DIALOG              102
+#define IDC_FLYAWAY_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_DIALOG_PARKING              134
+#define IDC_DIALOG_STORE                135
+#define IDD_FLYAWAY_DIALOG3             136
+#define IDC_DIALOG_FLIGHT               136
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
