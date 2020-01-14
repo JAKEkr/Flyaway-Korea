6a91eac6152da484473d38b1cf66cbee4f3d1773
Renz <soundarya.sejong@gmail.com>
2020-01-14 18:01:57 +0900

    Soundarya xml2json

--- Server/arrivals.js
+++ Server/arrivals.js
@@ -1,28 +1,30 @@
 //Arrivals
 require('dotenv').config()
-const convert = require('xml-js');
+const convert = require('xml2json');
 const fs=require('fs');
-//var xml = fs.readFileSync('test.xml');
-// var options = {compact:true , ignoreComment:true, spaces: 4};
-var options = {compact:true , ignoreComment:true, spaces: 4,textKey: null, attributesKey: '$', commentKey: 'value'};
-
-//var result=convert.xml2json(body, options);
-const cron=require('node-cron')
-
-const Agenda = require('agenda');
+const cron=require('node-cron');
 var Request = require("request");
+var options = { object: false,
+    reversible: false,
+    coerce: false,
+    sanitize: true,
+    trim: true,
+    arrayNotation: false,
+    alternateTextNode: false};
+
+
 Request.get("http://openapi.airport.kr/openapi/service/StatusOfPassengerFlights/getPassengerArrivals?ServiceKey=K3POVg15ZxYOa9zrxuVXDEuHy6bi8AZ9jcKR7QalKz2m7ZBBBv4wDYqIXuEy39oHLHo5rmWozqgEzVkSyK%2By2g%3D%3D", (error, response, body) => {
     if(error) {
         return console.dir(error);
     }
         cron.schedule('30 * * * * *',function(){
-            console.log('running test after 30s');
 
-            fs.writeFile("C:/Users/USER/Flyaway-Korea/Server/arrivalsJson.txt",(convert.xml2json(body, options)), function(err) {
+            fs.writeFile("C:/Users/USER/Flyaway-Korea/Server/arrivalsJson.txt",(convert.toJson(body, options)), function(err) {
                 if(err) {
                 return console.log(err);
             }
-            console.log("The file was saved!");
+            console.log("Arrivals updated after 30s");
+
         });
     }); 
 });
--- Server/parkinglot.js
+++ Server/parkinglot.js
@@ -1,25 +1,27 @@
-//parkinglot
+//Car
 require('dotenv').config()
-const convert = require('xml-js');
+const convert = require('xml2json');
 const fs=require('fs');
-//var xml = fs.readFileSync('test.xml');
-// var options = {compact:true , ignoreComment:true, spaces: 4};
-var options = {compact:true , ignoreComment:true, spaces: 4,textKey: null, attributesKey: '$', commentKey: 'value'};
-
-//var result=convert.xml2json(body, options);
-const cron=require('node-cron')
-
-const Agenda = require('agenda');
+const cron=require('node-cron');
 var Request = require("request");
+var options = {object: false,
+    reversible: false,
+    coerce: false,
+    sanitize: true,
+    trim: true,
+    arrayNotation: false,
+    alternateTextNode: false};
+
 Request.get("http://openapi.airport.kr/openapi/service/StatusOfParking/getTrackingParking?ServiceKey=K3POVg15ZxYOa9zrxuVXDEuHy6bi8AZ9jcKR7QalKz2m7ZBBBv4wDYqIXuEy39oHLHo5rmWozqgEzVkSyK%2By2g%3D%3D", (error, response, body) => {
     if(error) {
         return console.dir(error);
     }
-    fs.writeFile("C:/Users/USER/Flyaway-Korea/Server/parkinglotJson.txt",(convert.xml2json(body, options)), function(err) {
-        if(err) {
+    cron.schedule('30 * * * * *',function(){
+    fs.writeFile("C:/Users/USER/Flyaway-Korea/Server/parkinglotJson.txt",(convert.toJson(body, options)), function(err) {
+            if(err) {
                 return console.log(err);
-        }
-        console.log("The file was saved!");
+            }
+            console.log("Parking Lot updated after 30s");
+        }); 
     });
-
 });
